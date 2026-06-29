531421fa7d1895bde0f836ffb9af71a5f3794114
Swayam Gandhi <swayamgandhi6@gmail.com>
2026-06-29 14:03:00 +0530

    responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-'use client';
+﻿'use client';
 
 import React, { useState, useEffect } from 'react';
 import styles from './Hero.module.css';
@@ -14,20 +14,23 @@ const Hero = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000); // Change image every 5 seconds
+        }, 5000);
 
         return () => clearInterval(interval);
     }, []);
 
     return (
         <section className={styles.hero}>
-            {images.map((img, index) => (
-                <div
-                    key={img}
-                    className={`${styles.bgImage} ${index === currentImageIndex ? styles.active : ''}`}
-                    style={{ backgroundImage: `url('${img}')` }}
-                />
-            ))}
+            <div className={styles.imageWrapper}>
+                {images.map((img, index) => (
+                    <img
+                        key={img}
+                        src={img}
+                        alt={`hero banner ${index + 1}`}
+                        className={`${styles.bgImage} ${index === currentImageIndex ? styles.active : ''}`}
+                    />
+                ))}
+            </div>
         </section>
     );
 };
